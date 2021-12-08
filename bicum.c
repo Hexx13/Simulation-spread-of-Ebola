dec22616f1cd17ca1a3f77177e0ab52f69cbdc7d
orbitz4k <itzorbitz5@gmail.com>
2021-12-08 17:12:24 +0000

    message

---
+++ bicum.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void* remove(cell*){
+  //jordan function
+  if((char) cell ){
+     
+  }
+}
+
+void* death(cell*){ 
+ if((char) cell == 'D'){
+
+  }
+}
+
+void* contactInfection(cell*){
+   //jordan function
+ if((char) cell ){
+
+  }
+}
+
+void* vectorInfection(cell*){
+   //jordan function
+ if((char) cell){
+
+  }
+}
+
+void* spontaneousInfection(cell*){
+   //jordan function
+ if((char) cell){
+
+  }
+}
+void* recover(cell*){ 
+ if((char) cell == 'E'){
+
+  }
+}
+
+void* immunityState(cell*){
+  
+ if((char) cell){
+   
+  }
+}
