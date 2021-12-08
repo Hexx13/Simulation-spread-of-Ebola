e188e3457931e69cfddff2c8a19ca4bed10d7ecb
JB <B00128828@mytudublin.ie>
2021-12-08 16:09:21 +0000

    counters

--- parallecode.c
+++ parallecode.c
@@ -26,16 +26,52 @@ int count_live_neighbour_cell(int a[row][col], int r, int c){
     }
     return count;
 }
+
+int count_infect_neighbour_cell(int a[row][col], int r, int c){
+    int i, j, count=0;
+    for(i=r-1; i<=r+1; i++){
+        for(j=c-1;j<=c+1;j++){
+            if((i==r && j==c) || (i<0 || j<0) || (i>=row || j>=col)){
+                continue;
+            }
+            if(a[i][j]==2){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int count_recovered_neighbour_cell(int a[row][col], int r, int c){
+    int i, j, count=0;
+    for(i=r-1; i<=r+1; i++){
+        for(j=c-1;j<=c+1;j++){
+            if((i==r && j==c) || (i<0 || j<0) || (i>=row || j>=col)){
+                continue;
+            }
+            if(a[i][j]==3){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
  
 int main(){
        int a[row][col], b[row][col];
     int i,j;
     int neighbour_live_cell;
+    int neighbour_infect_cell;
+    int neighbour_recovered_cell;
+
+
  
     //generate matrix canvas with random values (live and dead cells)
     for(i=0; i<row; i++){
         for(j=0;j<col;j++){
-            a[i][j] = rand() % 2;
+            a[i][j] = rand() % 4;
         }
     }
      
@@ -51,10 +87,12 @@ int main(){
     }
  
     //next canvas values based on live neighbour count
-    for(int fanny = 10; fanny<20; fanny++){
+    for(int gen = 1; gen<10; gen++){
     for(i=0; i<row; i++){
         for(j=0;j<col;j++){
             neighbour_live_cell = count_live_neighbour_cell(a,i,j);
+            neighbour_infect_cell = count_infect_neighbour_cell(a, i, j);
+            neighbour_recovered_cell = count_recovered_neighbour_cell(a, i, j);
             if(a[i][j]==1 && (neighbour_live_cell==2 || neighbour_live_cell==3)){
                 b[i][j]=1;
                 a[i][j]=b[i][j];
