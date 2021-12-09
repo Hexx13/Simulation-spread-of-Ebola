9436063eb36a2555274f0fd16969ea1c55c9b793
Egino200 <71835027+Egino200@users.noreply.github.com>
2021-12-09 22:00:08 +0000

    changfed stuff

--- parallecode.c
+++ parallecode.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 //change row and column value to set the canvas size
 int row = 10;
 int col = 10;
+
+void stateFunc(void *cell);
+
 //creates row boundary
 int row_line(){
     printf("\n");
@@ -89,7 +93,7 @@ int count_cells(char a[row][col], int r, int c){
             }
         }
     }
-    printf("%d \n", count);
+   // printf("%d \n", count);
     return count;
 }
 
@@ -120,10 +124,10 @@ int main(){
     int total_infect_cell;
     int total_dead_cell;
     int total_cell;
-      printf("0 = dead\n");
-    printf("1 = alive\n");
-    printf("2 = infected\n");
-      printf("3 = recovered\n");
+      printf("D = dead\n");
+    printf("A = alive\n");
+    printf("C = infected\n");
+      printf("Y = immune\n");
 
  
     //generate matrix canvas with random values (live and dead cells)
@@ -162,21 +166,8 @@ int main(){
             vector_infect_cell = count_infect_vector_cell(a,i,j);
             total_dead_cell = count_dead_total_cell(a,i,j);
             neighbour_infect_cell = count_infect_neighbor_cell(a, i, j);
-            //neighbour_recovered_cell = count_recovered_neighbour_cell(a, i, j);
-            if(a[i][j]=='C' && (total_infect_cell==2 || total_infect_cell==3)){
-                b[i][j]='C';
-                a[i][j]=b[i][j];
-            }
- 
-            else if(a[i][j]=='A' && total_infect_cell==3){
-                b[i][j]='C';
-                a[i][j]=b[i][j];
-            }
- 
-            else{
-                b[i][j]='A';
-                a[i][j]=b[i][j];
-            }
+
+             stateFunc(&a[i][j]);
         }
     }
 
