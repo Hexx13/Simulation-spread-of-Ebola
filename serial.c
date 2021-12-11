4ea7a5913805da8d750ff24f558a1c8847fa0da4
Hexx13 <b00124650@mytudublin.ie>
2021-12-11 16:57:31 +0000

    removed debug prints for the array

--- serial.c
+++ serial.c
@@ -10,31 +10,12 @@ int rc = 10;
 char *populate(int *row, int *col, char a[*row][*col]);
 void transCell(int *rc,char a[*rc][*rc], char b[*rc][*rc], int startRow, int startCol, int endRow, int endCol);
 
-//creates row boundary
-int row_line() {
-    printf("\n");
-    for (int i = 0; i < col; i++) { printf(" -----"); }
-    printf("\n");
-}
-
-
 int main() {
     char a[rc][rc], b[row][col];
-    int i, j;
 
     //generate matrix canvas with random values (live and dead cells)
     populate(&row,&col,a);
 
-    //print array matrix
-    printf("Initial Stage:");
-    row_line();
-    for (i = 0; i < row; i++) {
-        printf(":");
-        for (j = 0; j < col; j++) {
-            printf("  %c  :", a[i][j]);
-        }
-        row_line();
-    }
 
     //next canvas values based on live neighbour count
     for (int gen = 1; gen < 5000; gen++) {
