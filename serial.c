ea9cc6b604858638ef113a478f8efdde65b8d81d
Hexx13 <b00124650@mytudublin.ie>
2021-12-11 17:07:10 +0000

    implement argv and argc to function

--- serial.c
+++ serial.c
@@ -4,22 +4,33 @@
 
 
 //change row and column value to set the canvas size
-int row = 10;
-int col = 10;
-int rc = 10;
+int row;
+int col;
+int rc;
+int time;
 char *populate(int *row, int *col, char a[*row][*col]);
 void transCell(int *rc,char a[*rc][*rc], char b[*rc][*rc], int startRow, int startCol, int endRow, int endCol);
 
 int main(int argc, char *argv[]) {
+    time = atoi(argv[1]);
+    row = atoi(argv[2]);
+    col = atoi(argv[2]);
+    rc = atoi(argv[2]);
     char a[rc][rc], b[row][col];
 
-    //generate matrix canvas with random values (live and dead cells)
+    //populate cell dimension
     populate(&row,&col,a);
 
 
-    //next canvas values based on live neighbour count
-    for (int gen = 1; gen < 5000; gen++) {
+    //Iterate time
+    for (int gen = 1; gen < time; gen++) {
+        //iterate through cells and assign to future array
         transCell(&rc, a, b, 0,0 ,row, col);
+
+        //dump here
+
+        //assign present array as future array
+        memcpy(a, b, sizeof(*a));
     }
     return 0;
 }
