cf294e04f352b1c72d4a86e2d320bd1428cf2a5b
Hexx13 <b00124650@mytudublin.ie>
2021-12-12 19:22:14 +0000

    Add parallel folder

---
+++ parrallel/populate.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void populate(int *row, int *col, char a[*row][*col]) {
+  int i;
+  int j;
+  for (i = 0; i < *row; i++) {
+    for (j = 0; j < *col; j++) {
+      // test code
+      a[i][j] = 'A';
+    }
+  }
+}
