11cdb3d1806206c666a9e7a35e85b0ff9b622bb4
Egino200 <71835027+Egino200@users.noreply.github.com>
2021-12-12 21:15:52 +0000

    visualisation file

--- visualisation.js
+++ visualisation.js
@@ -4,68 +4,84 @@ src="//cdn.jsdelivr.net/terra/latest/mainfile";
 
 //const r =1000;
 //const c = 1000;
-//var genCounter = 0;
-//var genTot = 500;
+//let genCounter = 0;
+//let genTot = 500;
 
 //using test data
 const r = 10;
 const c = 10;
-var genCounter=0;
-var genTot = 5;
+let genCounter=0;
+let genTot = 5;
 
 //Arrays recording number of each type each gen.
-var susArray = [];
-var infectArray = [];
-var deadArray = [];
-var recoveredArray = [];
+let susArray = [];
+let infectArray = [];
+let deadArray = [];
+let recoveredArray = [];
 
-var generationArray = [];
+let generationArray = [];
 
 //Array for Display of Simulation
-var rowArray = [];
+let rowArray = [];
 
 //Incrementing counters
-var susCount= 0;
-var infectCount= 0;
-var deadCount= 0;
-var recoveredCount= 0;
+let susCount= 0;
+let infectCount= 0;
+let deadCount= 0;
+let recoveredCount= 0;
 
 // read in the file
 //takes in the values of text.txt and splits them into differect generations using the '.' as an indicator, they are then saved to the generations array
 function load_file()
 {
 
-    var lineReader = require('line-reader');
+    let lineReader = require('line-reader');
 
     lineReader.eachLine('text.txt', function(line, last) {
 
-        let generations = line.split(".");
+       let generations = line.split(".");
 
-        for (var i = 0; i< generations.length;i++) {
-            var rowcol = Math.sqrt(generations[i].length);
+       let realGridArray;
+        for (let i = 0; i< generations.length;i++) {
+
+            let rowcol = Math.sqrt(generations[i].length);
             console.log(generations[i].length);
             console.log(rowcol);
-            for (var j = 0; j < generations[j].length; j++) {
 
-                for(var k = 0; k < rowcol ; k++){
-                    for (var l = 0; l < rowcol ; l++){
 
-                    }
-                }
-                var line =generations[j];
+            for (let j = 0; j < generations[j].length; j++) {
 
+                let line =generations[j];
                 // console.log(line.charAt(j));
+            }
+            let gridArray;
 
+            for(let j = 0; j < rowcol; j++ ){
+
+                for(let i = 0; i < rowcol; i++ ){
+                gridArray[j][i] = generations[i].charAt(i);
+
+                }
             }
         }
+
+        t.grid = t.makeGrid(gridArray);
+
+
+
         // console.log(generations[1]);
         // console.log(generations[2]);
         // console.log(generations[3]);
         //console.log(generations[4]);
         // console.log(generations[8]);
 
-   
+
     });
+
+
+}
+
+function createDrawGrid( ){
 
 
 }
@@ -80,7 +96,7 @@ function change(){
 }
 
     document.getElementById('text.txt').addEventListener('change', function() {
-        var fr=new FileReader();
+        let fr=new FileReader();
         fr.onload=function(){
             document.getElementById('output').textContent=fr.result; }
         fr.readAsText(this.files[0]);})
