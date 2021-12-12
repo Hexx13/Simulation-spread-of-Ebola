1fe4a19ca3675691bfff3971454fe1204cd27d6f
Egino200 <71835027+Egino200@users.noreply.github.com>
2021-12-12 15:53:06 +0000

    add code from test branch

--- Io.c
+++ Io.c
@@ -7,3 +7,17 @@ void IoWrite(int *rc, char *a[*rc][*rc]){
     fwrite(a,1, sizeof(*a), text);
     fclose(text);
 }
+/*
+code from test branch
+---------------------------------
+ #include <stdio.h>
+#include <stdlib.h>
+
+void IoWrite(int *rc, char a[*rc][*rc]){
+    FILE *text;
+    text=fopen( "text.txt", "a" );
+    fwrite(a,*rc, sizeof(a), text);
+    fclose(text);
+}
+ ---------------------------------
+*/
